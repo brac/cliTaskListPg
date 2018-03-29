ad1f2953e41830a60f0bde7093584979e0e50c72
Jared Grippe <jared@deadlyicon.com>
2018-03-29 11:07:59 -0700

    jareds refactoring

--- app/cliTaskListPG.js
+++ app/cliTaskListPG.js
@@ -1,76 +1,55 @@
 #!/usr/local/bin/node
 // jshint asi:true
 
-const db      = require('../database/queries')
-const command = process.argv[2]
-const task    = process.argv[3]
+const queries = require('../database/queries')
 
-taskList = (cmd, task) => {
-  switch(cmd){
-    case 'list':
-      db.list()
-      .then(res => {
-        // Print the header
-        console.log(`\nID Description \n-- ----------- `)
+const commands = {}
 
-        // Print the list
-        for (let i = 0; i < res.length; i++) {
-          console.log(`${res[i].id}  ${res[i].name}`)
+commands.list = () =>
+  queries.list().then(tasks => {
+    // Print the header
+    console.log(`ID Description Completed`)
+    console.log(`-- ----------- ---------`)
+    // Print the list
+    tasks.forEach(task => {
+      console.log(`${task.id}  ${task.name}  ${task.complete ? 'complete' : ''}`)
+    })
+    // Print the summation
+    console.log(`\nYou have ${tasks.length} tasks\n` )
+  })
+
+commands.add = (task) =>
+  queries.addTask(task).then(task => {
+    console.log(`Created task: ${task.id}`)
+  })
+
+
+commands.complete = (taskId) =>
+  queries.completeTask(taskId).then(task => {
+    console.log(`Completed task ${task.id}`)
+  })
+
+commands.delete = (taskId) =>
+  queries.deleteTask(taskId).then(task => {
+    if (!task) throw 'That task id has been deleted or is not present'
+    console.log(`Deleted task: ${task.name}`)
+  })
+
+{
+  const command = process.argv[2]
+  if (command in commands){
+    commands[command](process.argv[3])
+      .then(
+        () => {
+          process.exit(0)
+        },
+        error => {
+          console.error(`Error: ${error}`)
+          process.exit(1)
         }
-
-        // Print the summation
-        console.log(`\nYou have ${ res.length} tasks\n` )
-      })
-      .catch(err => { console.error(err.message)})
-    break
-
-    case 'add':
-      db.addTask(task)
-      .then(() => {
-        db.list()
-        .then(res => {
-          console.log(`Created task: ${res[res.length-1].id}`)
-        })
-        .catch(err => { console.error(err.message)})
-      })
-      .catch(err => { console.error(err.message)})
-    break
-
-    case 'complete':
-      db.list()
-      .then(res => {
-        let found = res.some(t => { return t.id == task})
-        if (!found) {
-          console.error('That task does not exist, please check the id you provided')
-        } else {
-          db.completeTask(task)
-          .then(() => {
-            let taskName = res.find(t => { return t.id == task })
-            console.log(`Completed task: ${ taskName.name }`)
-          })
-          .catch(err => console.error(err.message))
-        }
-      })
-      .catch(err => console.error(err.message))
-    break
-
-    case 'delete':
-      db.list()
-      .then(res => {
-        let found = res.some(t => { return t.id == task })
-        if (!found) { console.error('That task id has been deleted or is not present')
-      } else {
-        db.deleteTask(task)
-        .then(() => {
-          taskName = res.find(t => { return t.id == task})
-          console.log(`Deleted task: ${taskName.name}`)
-        })
-        .catch(err => console.error(err.message))
-      }
-      })
-      .catch(err => console.error(err.message))
-    break
+      )
+  }else{
+    console.error(`Error: unknown command "${command}"`)
+    process.exit(1)
   }
 }
-
-taskList(command, task)
